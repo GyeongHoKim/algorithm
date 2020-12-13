412d72ef6c4e382c17fb37a4add2bab7319e6626
김경호 <rlarudgh2017@gmail.com>
2020-12-13 14:42:51 +0000

    algospot, ID : FESTIVAL, reduced time(version 2)

--- algospot/FESTIVAL.cpp
+++ algospot/FESTIVAL.cpp
@@ -3,17 +3,6 @@
 #include <numeric>
 using namespace std;
 
-double even(vector<int>::iterator iter1, vector<int>::iterator iter2)
-{
-	/*
-	cout << "average among [";
-	for(vector<int>::iterator iter = iter1; iter <= iter2; iter++)
-		cout << *iter << ',';
-	cout << ']' << endl;
-	*/
-	return static_cast<double>(accumulate(iter1, iter2+1, 0.0)) / static_cast<double>(iter2 - iter1 + 1);
-}
-
 int main()
 {
 	int C,N,L;
@@ -29,11 +18,12 @@ int main()
 		double temp;
 		vector<int>::iterator iter1 = cost.begin();
 		for(; iter1 <= cost.end() - L; iter1++) {
+			temp = static_cast<double>(accumulate(iter1, iter1+L-1, 0.0));
 			for(
 		vector<int>::iterator iter2 = iter1+L-1; iter2 < cost.end(); iter2++) {
-				temp = even(iter1, iter2);
-				if(min > temp)
-					min = temp;
+				temp += static_cast<double>(*iter2);
+				if(min > temp/(iter2-iter1+1))
+					min = temp/(iter2-iter1+1);
 			}
 		}
 		
