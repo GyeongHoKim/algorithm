3bef654b902affb2379d2734c9ade53a367e3510
GyeongHo Kim <GyeongHoKim@users.noreply.github.com>
2020-12-31 18:07:59 +0900

    ADD BOJ 3085

--- source/GyeongHo/boj/3085.cpp
+++ source/GyeongHo/boj/3085.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-char table[50][50];
+char table[51][51];
+int maxCount = 0;
 
 void swap(char *c1, char *c2)
 {
@@ -10,13 +11,52 @@ void swap(char *c1, char *c2)
 	*c2 = tmp;
 }
 
-int chk(int row, int column, char color, int type)
+void countColumnCandy(int size)
 {
-	// type=0, compare row. type=1, compare column
-	int max = 0;
+	for(int i = 0; i < size; ++i) {
+		int count = 1;
+		for(int j = 0; j < size; ++j) {
+			if(table[i][j] == table[i][j + 1]) ++count;
+			else {
+				if(count > maxCount) maxCount = count;
+				count = 1;
+			}
+		}
+	}
 }
 
-int solution(int N);
+void countRowCandy(int size)
+{
+	for(int i = 0; i < size; ++i) {
+		int count = 1;
+		for(int j = 0; j < size; ++j) {
+			if(table[j + 1][i] == table[j][i]) ++count;
+			else {
+				if(count > maxCount) maxCount = count;
+				count = 1;
+			}
+		}
+	}
+}
+
+void bruteForce(int N)
+{
+	for(int i = 0; i < N; ++i) {
+		for(int j = 0; j < N; ++j) {
+			if(j != N) {
+				swap(table[i][j], table[i][j + 1]);
+				countRowCandy(N);
+				countColumnCandy(N);
+				swap(table[i][j + 1], table[i][j]);
+				
+				swap(table[j][i], table[j + 1][i]);
+				countRowCandy(N);
+				countColumnCandy(N);
+				swap(table[j + 1][i], table[j][i]);
+			}
+		}
+	}		
+}
 
 int main()
 {
@@ -29,7 +69,9 @@ int main()
 			for(int j = 0; j < N; ++j)
 				cin >> table[i][j];
 
-		cout << solution(N) << endl;
+		bruteForce(N);
+		
+		cout << maxCount << endl;
 	}
 
 	return 0;
