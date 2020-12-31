dae816ebb2415af3f326ee45f757e2024cf4aba9
GyeongHo <rlarudgh2017@gmail.com>
2020-12-31 20:49:50 +0900

    UPDATE PROGRAMMERS PRIMEFIND
    missed case of perfect squares
    added '=' at line number 10

--- source/GyeongHo/programmers/primeFind.cpp
+++ source/GyeongHo/programmers/primeFind.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool chk(int n)
 {
     if(n < 2) return false;
-    for(int i = 2; i*i < n; ++i)
+	for(int i = 2; i*i <= n; ++i)
         if(n % i == 0) return false;
 	return true;
 }
