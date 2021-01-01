591c7536b09b8364a10f29ea25224358e5642757
GyeongHo <rlarudgh2017@gmail.com>
2021-01-01 23:55:02 +0900

    ADD BOJ 10972

---
+++ source/GyeongHo/boj/10972.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	int arr[N];
+
+	for(int i = 0; i < N; ++i)
+		cin >> arr[i];
+
+	if(next_permutation(arr, arr + 4))
+		for(int i = 0; i < N; ++i)
+			cout << arr[i] << ' ';
+	else
+		cout << -1;
+
+	return 0;
+}
