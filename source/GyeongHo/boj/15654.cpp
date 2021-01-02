e3a7768b912ba9bbafd17020dcef715ea662587f
GyeongHo <rlarudgh2017@gmail.com>
2021-01-02 15:31:57 +0900

    ADD BOJ 15654

---
+++ source/GyeongHo/boj/15654.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+int arr[8];
+bool visited[8] = {false};
+
+void permutation(int N, int M)
+{
+	if(M == 0) {
+		for(vector<int>::const_iterator iter = v.begin(); iter < v.end(); ++iter)
+			cout << *iter << ' ';
+		cout << "\n";
+		return;
+	}
+
+	for(int i = 0; i < N; ++i) {
+		if(visited[i]) continue;
+		v.push_back(arr[i]);
+		visited[i] = true;
+		permutation(N, M - 1);
+		v.pop_back();
+		visited[i] = false;
+	}
+}
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+	for(int i = 0; i < N; ++i)
+		cin >> arr[i];
+
+	sort(arr, arr + N);
+
+	permutation(N, M);
+
+	return 0;
+}
