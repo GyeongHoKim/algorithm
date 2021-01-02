866cec404d9c66fa78c78073ca63ba08948eec4b
GyeongHo <rlarudgh2017@gmail.com>
2021-01-02 16:26:09 +0900

    ADD BOJ 15656

---
+++ source/GyeongHo/boj/15656.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+int arr[8];
+
+void permutation(int N, int M)
+{
+	if(M == 0) {
+		for(vector<int>::const_iterator iter = v.begin(); iter < v.end(); ++iter)
+			cout << *iter << ' ';
+		cout << "\n";
+		return;
+	}
+
+	for(int i = 0; i < N; ++i) {
+		v.push_back(arr[i]);
+		permutation(N, M - 1);
+		v.pop_back();
+	}
+}
+
+int main()
+{
+	//ios::sync_with_stdio(false);
+	//cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+	for(int i = 0; i < N; ++i)
+		cin >> arr[i];
+
+	sort(arr, arr + N);
+
+	permutation(N, M);
+
+	return 0;
+}
