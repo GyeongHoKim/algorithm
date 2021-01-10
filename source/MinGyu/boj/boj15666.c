3d5d31fd4e8e05362738a3cd7d9465b4bfeb6069
mingyu <ghfn123@gmail.com>
2021-01-10 00:29:17 +0000

    ADD BOJ 15663 - 15666

---
+++ source/MinGyu/boj/boj15666.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int list[8];
+int arr[8];
+int n, m;
+
+void backtrack(int cnt,int num);
+void sortbubble(int * arr)
+{
+  int temp;
+  int len = n;
+
+  for(int i=0; i<len -1; i++)
+  {
+    for(int j=0; j<(len-1)-i; j++)
+    {
+      if(arr[j]>arr[j+1])
+      {
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+}
+
+int main() {
+  
+  scanf("%d %d",&n ,&m);
+  for(int i =0; i<n; i++)
+    scanf("%d",&arr[i]);
+  sortbubble(arr);
+
+  backtrack(0,0);
+
+}
+
+void backtrack(int cnt,int num) {
+  if(cnt == m) {
+    for(int i=0; i<m; i++)
+    {
+      printf("%d ",list[i]);
+    }printf("\n");
+    return ;
+  }
+  
+  bool check[10001] = {false,};
+
+  for(int j=num; j<n; j++)
+  {
+    if(check[arr[j]]==false)
+    {
+        check[arr[j]] = true;
+        list[cnt] = arr[j];
+        backtrack(cnt+1,j);
+    }
+  }
+  return;
+}
