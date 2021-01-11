78db3a530adfc9370af2f988f054fa7a78c6a4f3
GyeongHoKim <rlarudgh2017@gmail.com>
2021-01-11 12:11:10 +0000

    ADD BOJ 1759

---
+++ source/GyeongHo/boj/1759.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int C, L;
+string words, answer;
+bool visit[15] = {false};
+
+bool chk()
+{
+	int vowel = 0;
+	int consonant = 0;
+	for(string::iterator iter = answer.begin(); iter < answer.end(); ++iter) {
+		if(*iter == 'a' || *iter == 'e' || *iter == 'i' || *iter == 'o' || *iter == 'u')
+			++vowel;
+		else
+			++consonant;
+		if(vowel >= 1 && consonant >= 2) return true;
+	}
+	return false;
+}
+
+void combination(int cnt)
+{
+	if(cnt == L) {
+		if(chk()) {
+			for(int i = 0; i < L; ++i)
+				cout << answer[i] << ' ';
+			cout << "\n";
+		}
+		return;
+	}
+
+	for(int i = 0; i < C; ++i) {
+		if(visit[i]) continue;
+		visit[i] = true;
+		answer.push_back(words[i]);
+		combination(cnt + 1);
+		visit[i] = false;
+		answer.pop_back();
+	}
+}
+
+int main()
+{
+	cin >> L >> C;
+	for(int i = 0; i < C; ++i) {
+		char tmp;
+		cin >> tmp;
+		answer.push_back(tmp);
+	}
+
+	combination(0);
+
+	return 0;
+}
