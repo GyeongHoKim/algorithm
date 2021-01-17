0c64aeed90a970b6d280fa3fe93f6c3c346f2da1
Jaeung <dlwodnd00@gmail.com>
2021-01-17 20:19:43 +0900

    ADD boj6603

--- source/JaeUng/boj6603.cpp
+++ source/JaeUng/boj6603.cpp
@@ -17,15 +17,15 @@ int main()
         for (int i = 1; i <= k; i++)
         {
             cin >> num[i];
-            check[i] = i > 6 ? 0 : 1;
+            check[i] = i > 6 ? 0 : 1; // 111111 로 시작하는 배열 만들기
         }
         do {
             for (int i = 1; i <= k; i++)
             {
-                if (check[i]) { cout << num[i] << ' '; }
+                if (check[i]) { cout << num[i] << ' '; } // 배열 값이 1이라면 num을 출력
             }
             cout << '\n';
-        } while (prev_permutation(check + 1, check + 1 + k));
+        } while (prev_permutation(check + 1, check + 1 + k)); // 1111110...0부터 0...0111111까지의 순열 만들기
         cout << '\n';
     } while(k);
 }
