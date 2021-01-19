51933d73c12c57ea80851fc5f66d980cf676d5ec
GyeongHoKim <rlarudgh2017@gmail.com>
2021-01-19 12:02:19 +0900

    ADD BOJ 1463

---
+++ source/GyeongHo/boj/1463.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace evaluateMIN
+{
+	class makeInOne
+	{
+		static int[] cache = new int[1000000];
+
+		static int dfs(int x)
+		{
+			if(cache[x] != -1) return cache[x];
+			else if(x == 1) return cache[x] = 0;
+			else if(x % 3 == 0) return cache[x] = 1 + dfs(x / 3);
+			else if(x % 2 == 0) return cache[x] = 1 + dfs(x / 2);
+			else return cache[x] = 1 + dfs(x - 1);
+		}
+
+		static void Main()
+		{
+			for(int i = 0; i < 1000000; ++i)
+				cache[i] = -1;
+
+			int x;
+			x = int.Parse(Console.ReadLine());
+
+			int MIN = dfs(x);
+			for(int i = 0; i < x; ++i) {
+				int tmp = i + dfs(x - i);
+				if(MIN > tmp)
+					MIN = tmp;
+			}
+
+			Console.Write(MIN);
+		}
+	}
+}
