0efbcabaab9d2fcef908e197620a09a1702b0e27
GyeongHoKim <rlarudgh2017@gmail.com>
2021-01-19 23:11:56 +0900

    ADD BOJ 11726

---
+++ source/GyeongHo/boj/11726.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int cache[1001];
+
+int set(int n)
+{
+	int& ret = cache[n];
+	if(ret != -1) return ret;
+	else return ret = set(n - 1) + set(n - 2);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	memset(cache, -1, sizeof(cache));
+	cache[1] = 1;
+	cache[2] = 2;
+
+	cout << set(n);
+	return 0;
+}
