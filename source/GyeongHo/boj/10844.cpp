65ac848ed4775214da5e44e9732ebc1bf27300a5
GyeongHoKim <rlarudgh2017@gmail.com>
2021-01-22 20:48:33 +0900

    UPDATE 10844
    it cannot solve only with length of the number
    should use the last number of the number

--- source/GyeongHo/boj/10844.cpp
+++ source/GyeongHo/boj/10844.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int cache[101];
+int cache[101][10];
+int MOD = 1000000000;
 
-int DP(int n)
+int DP(int n, int L)
 {
-	if(cache[n] != 0) return cache[n];
-	return cache[n] = (DP(n - 1) * 2 - 1) % 1000000000;
+	int& ret = cache[n][L];
+	if(ret != -1) return ret;
+	if(L == 9) return ret = cache[n-1][L-1] % MOD;
+	if(L == 0) return ret = cache[n-1][L+1] % MOD;
+	return (cache[n-1][L-1] + cache[n-1][L+1]) % MOD;
 }
 
 int main()
 {
 	int n;
 	cin >> n;
-	cache[1] = 9; cache[2] = 17;
+	memset(cache, -1, sizeof(cache));
+	for(int i = 1; i < 10; ++i)
+		cache[1][i] = 1;
+	cache[1][0] = 0;
 
-	cout <<DP(n);
+	int sum = 0;
+	for(int i = 0; i < 10; ++i)
+		sum += DP(n, i);
+	cout << sum;
 	return 0;
 }
