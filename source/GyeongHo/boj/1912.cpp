109be52f6e1f5ffd15c387192ea040158b8eba38
GyeongHoKim <rlarudgh2017@gmail.com>
2021-01-25 20:29:57 +0900

    ADD BOJ 1912

---
+++ source/GyeongHo/boj/1912.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int arr[100001], cache[100001];
+
+int DP(int n)
+{
+	for(int i = 2; i <= n; ++i)
+		cache[i] = max(arr[i], cache[i - 1] + arr[i]);
+
+	int MAX = -1;
+	for(int i = 1; i <= n; ++i)
+		MAX = max(MAX, cache[i]);
+
+	return MAX;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	for(int i = 1; i <= n; ++i)
+		cin >> arr[i];
+
+	cache[1] = arr[1];
+
+	cout << DP(n);
+	return 0;
+}
