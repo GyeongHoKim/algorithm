c9a07ad23b56a030f0fe13270fd7671a040a38b2
GyeongHoKim <rlarudgh2017@gmail.com>
2021-02-05 08:02:46 +0000

    ADD BOJ 1932

---
+++ source/GyeongHo/boj/1932.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int N;
+int D[501][501], arr[501][501];
+
+int DP(int i, int j)
+{
+	int& ret = D[i][j];
+	if(ret != -1) return ret;
+	return ret = max(arr[i][j] + DP(i + 1, j), arr[i][j] + DP(i + 1, j + 1));
+}
+
+int main()
+{
+	memset(D, -1, sizeof(D));
+	cin >> N;
+	for(int i = 1; i <= N; ++i)
+		for(int j = 1; j <= i; ++j)
+			cin >> arr[i][j];
+
+	cout << DP(1, 1);
+	return 0;
+}
