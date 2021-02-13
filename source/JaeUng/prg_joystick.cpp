1ed9dabbb4c386ca58b6c55bd051bb1ad8c234bd
Jaeung <dlwodnd00@gmail.com>
2021-02-13 14:21:38 +0900

    modify

--- source/JaeUng/prg_joystick.cpp
+++ source/JaeUng/prg_joystick.cpp
@@ -29,8 +29,8 @@ int solution(string name)
 
 int main()
 {
-    //ios::sync_with_stdio(false);
-    //cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
     string name;
     cin >> name;
--- source/JaeUng/prg_체육복.cpp
+++ source/JaeUng/prg_체육복.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <iostream>
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
@@ -31,5 +32,21 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
             }
         }
     }
-    return answer;
+    return n - answer;
+}
+
+int main()
+{
+    int n, s, r;
+    cin >> n >> s >> r;
+
+    vector<int> lost(s), reserve(r);
+    for(int i = 0; i < s; i++) 
+        cin >> lost[i];
+
+    for(int i = 0; i < r; i++) 
+        cin >> reserve[i];
+
+    cout << solution(n, lost, reserve);
+
 }
