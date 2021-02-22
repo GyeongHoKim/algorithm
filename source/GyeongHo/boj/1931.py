cc1d8613110ea358f383168020a883bb08a84f10
GyeongHo Kim <GyeongHoKim@users.noreply.github.com>
2021-02-22 18:37:52 +0900

    UPDATE BOJ 1931
    sort by second parameter to reduce unnecessary computation

--- source/GyeongHo/boj/1931.py
+++ source/GyeongHo/boj/1931.py
@@ -1,22 +1,20 @@
 n = int(input())
 timeList = []
-for i in range(n):
-	a, b = map(int, input().split())
-	timeList.append((a, b))
-
+for _ in range(n):
+    a, b = map(int, input().split())
+    timeList.append((b, a))
 timeList.sort()
 
-def solution(nextIdx, prevIdx, count):
-	while nextIdx < n:
-		if timeList[nextIdx][0] < timeList[prevIdx][1]:
-			nextIdx += 1
-		else:
-			prevIdx = nextIdx
-			nextIdx += 1
-			count += 1
-	return count
+def solution(prev, start, count):
+    while start < n:
+        while timeList[prev][0] > timeList[start][1] and start < n:
+            start += 1
+        if start == n:
+            return count
+        else:
+            count += 1
+            prev = start
+            start += 1
+    return count
 
-countList = [0] * n
-for i in range(n):
-	countList[i] = solution(i + 1, i, 1)
-print(max(countList))
+print(solution(0, 1, 1))
