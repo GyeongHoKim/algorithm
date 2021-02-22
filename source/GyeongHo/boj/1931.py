c6995ba58a1efbdb68ced3449f9ee9b8a7d3b47d
GyeongHoKim <rlarudgh2017@gmail.com>
2021-02-22 20:14:37 +0900

    UPDATE BOJ 1931

--- source/GyeongHo/boj/1931.py
+++ source/GyeongHo/boj/1931.py
@@ -1,20 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-timeList = []
-for _ in range(n):
-    a, b = map(int, input().split())
-    timeList.append((b, a))
-timeList.sort()
+timeList = [[0] * 2 for _ in range(100000)]
+for i in range(n):
+    timeList[i][0], timeList[i][1] = map(int, input().split())
+timeList.sort(key = lambda x: (x[1], x[0]))
 
-def solution(prev, start, count):
-    while start < n:
-        while timeList[prev][0] > timeList[start][1] and start < n:
-            start += 1
-        if start == n:
-            return count
-        else:
-            count += 1
-            prev = start
-            start += 1
-    return count
-
-print(solution(0, 1, 1))
+count = 1
+endTime = timeList[0][1]
+for i in range(1, n):
+	if timeList[i][0] >= endTime:
+		count += 1
+		endTime = timeList[i][1]
+print(count)
