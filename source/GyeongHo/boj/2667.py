bdf2a4472cd7d2a603ed3b9bcddc44fb2923bfcb
GyeongHoKim <rlarudgh2017@gmail.com>
2021-03-09 17:52:04 +0900

    ADD BOJ 2667

--- source/GyeongHo/boj/2667.py
+++ source/GyeongHo/boj/2667.py
@@ -1,42 +1,36 @@
 n = int(input())
-MAP = [list(map(int, input())) for _ in range(n)]
-visited = [[False] * n for _ in range(n)]
-town = 0
+square = [list(map(int, input())) for _ in range(n)]
 house = []
-dirY = [0, 0, 1, -1]
-dirX = [1, -1, 0, 0]
+town = 0
+dy = [0, 0, 1, -1]
+dx = [1, -1, 0, 0]
 
 def chk(i, j):
-    if 0 <= i < n and 0 <= j < n and not visited[i][j] and MAP[i][j] == 1:
-        return True
-    return False
+	if 0 <= i < n and 0 <= j < n and square[i][j] == 1:
+		return True
+	return False
 
-def bfs(i, j):
-    q = [(i, j)]
-    visited[i][j] = True
-    MAX = 0
-    print(q)
-    while q:
-        posY, posX = q.pop(0)
-        for i in range(4):
-            newY = posY + dirY[i]
-            newX = posX + dirX[i]
-            if chk(newY, newX):
-                MAP[newY][newX] = MAP[posY][posX] + 1
-                q.append((newY, newX))
-                visited[newY][newX] = True
-                MAX = max(MAX, MAP[newY][newX])
-    return MAX
+def bfs(y, x, count):
+	q = [(y, x)]
+	square[y][x] = 0
+	while q:
+		posY, posX = q.pop(0)
+		for i in range(4):
+			newY = posY + dy[i]
+			newX = posX + dx[i]
+			if chk(newY, newX):
+				q.append((newY, newX))
+				square[newY][newX] = 0
+				count += 1
+	return count
 
 for i in range(n):
-    for j in range(n):
-        if MAP[i][j] == 1:
-            house.append(bfs(i, j))
-            town += 1
-
-print(MAP)
+	for j in range(n):
+		if square[i][j] == 1:
+			house.append(bfs(i, j, 1))
+			town += 1
 
 house.sort()
 print(town)
 for i in house:
-    print(i)
+	print(i)
