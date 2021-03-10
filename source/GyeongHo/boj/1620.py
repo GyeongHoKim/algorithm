4ae037d3185383b4347ec6d5cddd6aa6a3100ff1
GyeongHoKim <rlarudgh2017@gmail.com>
2021-03-10 20:51:03 +0900

    UPDATE BOJ 1620

--- source/GyeongHo/boj/1620.py
+++ source/GyeongHo/boj/1620.py
@@ -2,15 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-d = {}
-for i in range(1, n + 1):
-	d[i] = input().strip()
+pkmName = [0]
+pkmNum = {}
+
+for i in range(n):
+	tmp = input().rstrip()
+	pkmNum[tmp] = i + 1
+	pkmName.append(tmp)
 
 for _ in range(m):
-	question = input().strip()
-	if question.isalpha():
-		for number, name in d.items():
-			if name == question:
-				print(number)
+	question = input().rstrip()
+	if question.isdigit():
+		print(pkmName[int(question)])
 	else:
-		print(d[int(question)])
+		print(pkmNum[question])
