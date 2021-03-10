a99ecf5cdd9de54bfb4bc06189fc14d7b3f550de
GyeongHoKim <rlarudgh2017@gmail.com>
2021-03-10 12:35:25 +0900

    ADD BOJ 11724

---
+++ source/GyeongHo/boj/11724.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+visited = [False] * n
+component = 0
+graph = [[] for _ in range(n)]
+for i in range(m):
+	start, end = map(int, input().split())
+	graph[start - 1].append(end - 1)
+
+def dfs(v):
+	visited[v] = True
+	for i in graph[v]:
+		if not visited[i]:
+			dfs(i)
+
+for i in range(n):
+	if not visited[i]:
+		dfs(i)
+		component += 1
+
+print(component)
