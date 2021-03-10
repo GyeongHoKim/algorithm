4aeb4eae6d68f0e7b5a6c98c2e3a55ce0b5ee8fb
GyeongHoKim <rlarudgh2017@gmail.com>
2021-03-10 19:46:00 +0900

    ADD BOJ 1620

---
+++ source/GyeongHo/boj/1620.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+d = {}
+for i in range(1, n + 1):
+	d[i] = input()
+
+for _ in range(m):
+	question = input()
+	if question.isalpha():
+		for number, name in d.items():
+			if name == question:
+				print(number)
+	else:
+		print(d[int(question)])
