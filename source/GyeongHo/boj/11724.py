de79620202436648e29835b030e46bba8fd43f86
GyeongHoKim <rlarudgh2017@gmail.com>
2021-03-10 15:23:05 +0900

    UPDATE BOJ 11724

--- source/GyeongHo/boj/11724.py
+++ source/GyeongHo/boj/11724.py
@@ -5,6 +5,7 @@ graph = [[] for _ in range(n)]
 for i in range(m):
 	start, end = map(int, input().split())
 	graph[start - 1].append(end - 1)
+	graph[end - 1].append(start - 1)
 
 def dfs(v):
 	visited[v] = True
