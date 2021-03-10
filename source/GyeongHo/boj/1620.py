0fd10cea5e91b6b53f1b0fd4ca4e2dcea57fa0cf
GyeongHoKim <rlarudgh2017@gmail.com>
2021-03-10 20:39:21 +0900

    UPDATE BOJ 1620

--- source/GyeongHo/boj/1620.py
+++ source/GyeongHo/boj/1620.py
@@ -4,10 +4,10 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 d = {}
 for i in range(1, n + 1):
-	d[i] = input()
+	d[i] = input().strip()
 
 for _ in range(m):
-	question = input()
+	question = input().strip()
 	if question.isalpha():
 		for number, name in d.items():
 			if name == question:
