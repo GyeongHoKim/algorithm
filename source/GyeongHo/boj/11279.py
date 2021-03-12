acec016747ab283fbec8416eae94d6d59d0c5090
GyeongHoKim <rlarudgh2017@gmail.com>
2021-03-12 04:46:12 +0000

    ADD BOJ 11279

---
+++ source/GyeongHo/boj/11279.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+q = []
+n = int(input())
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if not q:
+            print(0)
+        else:
+            print(heapq.heappop(q) * -1)
+    else:
+        heapq.heappush(q, x * -1)
