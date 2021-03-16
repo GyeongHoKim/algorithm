7a881d05781fe864910bfbdf9022c1fd9b8ce9f6
GyeongHoKim <rlarudgh2017@gmail.com>
2021-03-16 17:27:44 +0900

    ADD BOJ 2875

---
+++ source/GyeongHo/boj/2875.py
@@ -0,0 +1,19 @@
+def timsko(m, n, k):
+	if m % 2 == 1:
+		k -= 1
+		m -= 1
+	while k > 0:
+		if m // 2 > n:
+			m -= 2
+			k -= 2
+		elif n > m // 2:
+			n -= 1
+			k -= 1
+		else:
+			m -= 2
+			n -= 1
+			k -= 3
+	return n
+
+m, n, k = map(int, input().split())
+print(timsko(m, n, k))
