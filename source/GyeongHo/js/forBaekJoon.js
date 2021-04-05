a8a91e779209d0b436bda1014a3eeaa68174fb28
GyeongHo <rlarudgh2017@gmail.com>
2021-04-05 20:48:38 +0900

    ADD JS BAEKJOONINPUT

--- source/GyeongHo/js/forBaekJoon.js
+++ source/GyeongHo/js/forBaekJoon.js
@@ -10,6 +10,11 @@ for (let i = 1; i < input.length; i++) {
   }
 }
 
+/*여기까지 했을 때,
+count에는 숫자 하나 그리고 numbers에는 [[], [], []]이런 식으로 줄별로 나뉘어서 들어감.
+첫 번째 줄에 테스트케이스의 개수, 그 뒤로 각각의 테스트케이스가 주어진다는 것을 가정하고 쓴 코드임.
+*/
+
 for (let i = 0; i < numbers.length; i++){
   let num1 = Number(numbers[i][0]);
   let num2 = Number(numbers[i][1]);
