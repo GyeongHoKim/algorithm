ebd7d20b809458e06590c6e031528da9a5f0d462
GyeongHoKim <rlarudgh2017@gmail.com>
2021-06-25 20:31:25 +0900

    update boj 1074
    no reason to make array
    prunning with y, x

--- source/GyeongHo/boj/1074.py
+++ source/GyeongHo/boj/1074.py
@@ -1,21 +1,31 @@
-count = 0
-def dfs(n, i, j, arr):
-	global count
-	if n <= 1:
-		for y in range(i, i + 2):
-			for x in range(j, j + 2):
-				arr[y][x] = count
-				count += 1
-		for i in arr:
-			print(i)
-		print()
-		return
-	dfs(n - 1, i, j, arr)
-	dfs(n - 1, i, j + 2 ** (n - 1), arr)
-	dfs(n - 1, i + 2 ** (n - 1), j, arr)
-	dfs(n - 1, i + 2 ** (n - 1), j + 2 ** (n - 1), arr)
-
 n, r, c = map(int, input().split())
-arr = [[0] * (2 ** n) for _ in range(2 ** n)]
-dfs(n, 0, 0, arr)
-print(arr[r][c])
+cnt = -1
+flag = False
+
+
+def z(n, y, x):
+    global cnt, r, c, flag
+    if flag:
+        return
+    if not (y <= r < y + 2 ** n and x <= c < x + 2 ** n):
+        cnt += (2 ** n) ** 2
+        return
+    elif n == 1:
+        for i in range(y, y + 2):
+            for j in range(x, x + 2):
+                cnt += 1
+                if r == i and c == j:
+                    flag = True
+                    return
+
+    z(n - 1, y, x)
+    z(n - 1, y, x + 2 ** (n - 1))
+    z(n - 1, y + 2 ** (n - 1), x)
+    z(n - 1, y + 2 ** (n - 1), x + 2 ** (n - 1))
+
+
+z(n, 0, 0)
+if flag:
+	print(cnt)
+else:
+	print('fuck')
