ad023ec5fa0d46a9b45712895f79d24905b449e2
GyeongHoKim <rlarudgh2017@gmail.com>
2022-01-17 10:11:35 +0900

    fix boj 15652

--- source/GyeongHo/boj/15652.cpp
+++ source/GyeongHo/boj/15652.cpp
@@ -1,40 +1,28 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-vector<int> v;
-bool chosen[9] = {false};
+int arr[7];
+int n, m;
 
-void combination(int N, int M)
+void dfs(int cnt, int prev)
 {
-	if(M == 0) {
-		for(vector<int>::iterator iter = v.begin(); iter < v.end(); ++iter)
-			cout << *iter << ' ';
-		cout << "\n";
+	if (cnt >= m) {
+		for (int i = 0; i < m; ++i)
+			cout << arr[i] << ' ';
+		cout << '\n';
 		return;
 	}
-
-	int index;
-	for(index = 8; index > 0; --index) {
-		if(chosen[index]) break;
+	for (int i = prev; i <= n; ++i) {
+		arr[cnt] = i;
+		dfs(cnt + 1, i);
 	}
-
-	for(int i = index; i < N + 1; ++i) {
-		chosen[i] = true;
-		v.push_back(i);
-		combination(N, M - 1);
-		chosen[i] = false;
-		v.pop_back();
-	}
-	return;
 }
 
 int main()
 {
-	int N, M;
-	chosen[1] = true;
-	cin >> N >> M;
-	combination(N, M);
-
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m;
+	dfs(0, 1);
+	
 	return 0;
 }
