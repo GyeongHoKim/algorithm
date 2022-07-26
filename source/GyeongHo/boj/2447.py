e80f1f670c8e9c02ae23e233373c1a923bafff33
GyeongHoKim <rlarudgh2017@gmail.com>
2022-07-26 14:34:00 +0900

    fixed boj 2447
    overide print with sys.stdout.write because of fast printing

--- source/GyeongHo/boj/2447.py
+++ source/GyeongHo/boj/2447.py
@@ -1,13 +1,17 @@
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+
 def star(i, j, n):
-    if ((i / n) % 3 == 1 and (j / n) % 3 == 1):
-        print(' ', end='')
+    if ((i // n) % 3 == 1 and (j // n) % 3 == 1):
+        print(' ')
     elif (n == 1):
-        print('*', end='')
+        print('*')
     else:
-        star(i, j, n / 3)
+        star(i, j, n // 3)
 
 n = int(input())
 for i in range(n):
     for j in range(n):
         star(i, j, n)
-    print()
+    print('\n')
