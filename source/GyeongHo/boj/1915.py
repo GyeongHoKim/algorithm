92e7dc476940fe5dc7b520bc99facf2ac7ef5819
GyeongHoKim <rlarudgh2017@gmail.com>
2022-11-12 16:13:54 +0900

    add boj 1915

---
+++ source/GyeongHo/boj/1915.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+arr = []
+for i in range(n):
+    tmp = list(map(int, list(input())))
+    arr.append(tmp)
+
+dp = [[0] * m for _ in range(n)]
+    
+ans = 0
+for i in range(n):
+    for j in range(m):
+        if i == 0 or j == 0:
+            dp[i][j] = arr[i][j]
+        elif arr[i][j] == 0:
+            dp[i][j] = 0
+        else:
+            dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+        ans = max(ans, dp[i][j])
+
+print(ans * ans)
